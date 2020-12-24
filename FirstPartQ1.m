4a0e21624219048d97a2cb46a57b35ac7f3cf477
MostafaAdel-cs <es-mostafaadel2023@alexu.edu.eg>
2020-12-24 13:49:09 +0200

    number b and c solved

--- FirstPartQ1.m
+++ FirstPartQ1.m
@@ -1,6 +1,36 @@
 %number a u(t+1) -u(t-3) means from -1 to 3
-x=linspace(-1,3,5000);
-yOfT=exp(-abs(x)/5);
 
-%plot(x,yOfT);
-%remove comment to activate plot
+x=linspace(-1,3,5000);
+
+
+%5000 point 1000 for each point  
+%for ex from -1 to 0 there is 1000 point to make function look smoother
+
+yOfT=FunctionTForQ1(x);
+
+subplot(2,2,1);
+plot(x,yOfT);
+title('Q)a)');
+
+
+
+%==========================================================================
+%number b compress the function by 3
+subplot(2,2,2);
+x3=x/3;
+yOf3T=FunctionTForQ1(x3);
+plot(x3,yOfT);
+title('Q)b)');
+
+%==========================================================================
+%number c shift the signal by 2 in advance
+
+subplot(2,2,3);
+yOfShift2=FunctionTForQ1(x);
+xShiftOf2=x-2;
+plot(xShiftOf2,yOfShift2);
+title('Q)c)')
+
+%==========================================================================
+%number d 
+
---
+++ FunctionTForQ1.m
@@ -0,0 +1,4 @@
+function [y] = FunctionTForQ1(x)
+  y=exp(-abs(x)/5);
+  
+end
